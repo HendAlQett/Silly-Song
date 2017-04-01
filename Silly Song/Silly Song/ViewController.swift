910becdfed9107e681765513fcdee27ec2e4d871
Hend Mahmoud <hend.alqett@gmail.com>
2017-04-02 00:20:43 +0200

    feat: Adding Outlets and Actions and generate silly song

--- Silly Song/Silly Song/ViewController.swift
+++ Silly Song/Silly Song/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joinWithSeparator( "\n")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +27,30 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text=""
+    }
+
+    @IBAction func displayLyrics(sender: AnyObject) {
+        let result = lyricsForName(bananaFanaTemplate, fullName: nameField.text!)
+        lyricsView.text = result
+    }
+    
+    func shortNameFromName(name: String) -> String {
+        var shortName = name.lowercaseString
+        shortName.removeAtIndex(shortName.startIndex)
+        print(shortName)
+        return shortName
+    }
+   
+    
+    func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+        let someName = shortNameFromName(fullName)
+        let lyrics = lyricsTemplate.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString:fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: someName)
+        
+        return lyrics
     }
 
 
